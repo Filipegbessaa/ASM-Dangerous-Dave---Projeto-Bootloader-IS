6bf32a58c16312bd2e5da5b628248baa4d48711d
filipe Bessa <filipegbessa@gmail.com>
2021-03-01 03:32:15 -0300

    Draw right wall

--- kernel.asm
+++ kernel.asm
@@ -3,6 +3,7 @@ jmp 0x0000:start
 
 data:
     press_enter db 'Press ENTER to start',0
+    dangerous_dave db 'Dangerous Dave',0
     ground db '----------------------------------------',0
 
     ;dados do projeto
@@ -54,6 +55,39 @@ prints:             ; mov si, string
   .endloop:
   ret
 
+start_screen:
+        ; set the cursor to top left-most corner of screen
+    mov dx, 0 
+    mov bh, 0      
+    mov ah, 0x2
+    int 0x10    
+    ; print 2000 blank chars to clean  
+    mov cx, 2000 
+    mov bh, 0
+    mov al, 0x20 ; blank char
+    mov ah, 0x9
+    int 0x10
+
+    mov dl, 0x0E
+    mov dh, 0x02    
+    mov bh, 0      
+    mov ah, 0x2
+    int 0x10
+    mov si, dangerous_dave
+    call prints
+
+    ; reset cursor to the center of the screen
+    mov dl, 0x0B
+    mov dh, 0x0C
+    mov bh, 0      
+    mov ah, 0x2
+    int 0x10
+
+    mov si, press_enter    ;si aponta para o começo do endereço onde está mensagem
+    call prints         ;Como só é impresso um caractere por vez, pegamos uma string com N caracteres e printamos um por um em ordem até chegar ao caractere de valor 0 que é o fim da string, assim prints pega a string para qual o ponteiro si aponta e a imprime na tela até o seu final
+
+    ret
+
 start_game:
     .loop2:
         call getchar    ;Recebe caractere do usuário
@@ -72,7 +106,7 @@ draw_ground:
     mov ah, 0x2
     int 0x10
 
-    mov bl, 0x06
+    mov bl, 0x04
     mov si, ground
     call prints
 
@@ -83,6 +117,59 @@ draw_ground:
     int 0x10
 
     ret
+
+draw_left_wall:
+    mov dl, 0
+    mov dh, 0
+    .loop
+        mov bh, 0      
+        mov ah, 0x2
+        int 0x10
+
+        mov al, '|'
+        mov bl, 0x04
+        call putchar
+        inc dh
+        cmp dh, 24
+        je .endloop
+        jmp .loop
+    
+    .draw_pipe:
+        mov dl, 1
+        mov dh, 20
+        mov bh, 0      
+        mov ah, 0x2
+        int 0x10
+
+        mov bl, 07      ;Draw the top of the pipe
+        mov al, '-'
+        call putchar
+        inc dl
+        call putchar
+
+        inc dl
+        inc dh          
+        mov bh, 0       ;Draw the pipe outlet
+        mov ah, 0x2
+        int 0x10
+        mov al, 'O'
+        call putchar
+
+        mov dh, 22
+        mov dl, 1
+        mov bh, 0      ;Draw the bottom of the pipe
+        mov ah, 0x2
+        int 0x10
+        mov al, '-'
+        call putchar
+        inc dl 
+        call putchar
+        
+    .endloop
+    jmp .draw_pipe
+    ret
+    
+
 
 draw_dave:
 
@@ -110,13 +197,12 @@ start:
     mov bl, 0x0E
     call clear
 
-    ;Imprimindo na tela a mensagem declarada em data
-    mov si, press_enter    ;si aponta para o começo do endereço onde está mensagem
-    call prints         ;Como só é impresso um caractere por vez, pegamos uma string com N caracteres e printamos um por um em ordem até chegar ao caractere de valor 0 que é o fim da string, assim prints pega a string para qual o ponteiro si aponta e a imprime na tela até o seu final
+    call start_screen
     call start_game
 
 
     call draw_ground
+    call draw_left_wall
     call draw_dave
 
 
